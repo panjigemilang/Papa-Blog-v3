00af21673b67919506f87ad43cc147a1b29d1536
nadirbasalamah <nadir008basalamah@gmail.com>
2020-12-29 05:06:46 +0700

    fix(post): Response fixes for post data. Memperbaiki respons untuk data post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\PostResource;
 use App\Post;
 use Illuminate\Support\Facades\DB;
 
@@ -10,23 +9,72 @@ class PostController extends Controller
 {
     public function getPosts($num)
     {
+        $status = "error";
+        $data = [];
+        $code = 404;
+        $message = "posts data not found";
+
         $criteria = Post::paginate($num);
-        return new PostResource($criteria);
+        if (sizeof($criteria) != 0) {
+            $status = "success";
+            $code = 200;
+            $message = "posts data found";
+            $data = $criteria->toArray();
+        }
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $code);
     }
 
     public function getPost($id)
     {
+        $status = "error";
+        $data = [];
+        $code = 404;
+        $message = "post data not found";
+
         $criteria = Post::where('id', $id)->get();
-        return new PostResource($criteria);
+        if (sizeof($criteria) != 0) {
+            $status = "success";
+            $code = 200;
+            $message = "post data found";
+            $data = $criteria->toArray();
+        }
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $code);
     }
 
     public function searchPost($title)
     {
+        $status = "error";
+        $data = [];
+        $code = 404;
+        $message = "posts data not found";
+
         $criteria = DB::table('posts')
             ->select('*')
             ->where('posts.title', 'LIKE', "%" . $title . "%")
             ->orderBy('posts.id', 'DESC')
             ->get();
-        return new PostResource($criteria);
+
+        if (sizeof($criteria) != 0) {
+            $status = "success";
+            $code = 200;
+            $message = "posts data found";
+            $data = $criteria->toArray();
+        }
+
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ], $code);
     }
 }
